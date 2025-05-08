4899294b3d58353fba40f88b5a391c223940cf92
Samuel Moore <samoore@student.42london.com>
2025-05-08 13:54:19 +0100

    fix norme errors and move mlx to mlx directory

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: samoore <samoore@student.42london.com>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/08 12:24:04 by samoore           #+#    #+#              #
-#    Updated: 2025/05/08 13:36:11 by samoore          ###   ########.fr        #
+#    Updated: 2025/05/08 13:53:39 by samoore          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,8 +19,8 @@ SRC_FILES = main.c
 OBJ_FILES = $(patsubst %.c, $(OBJ_DIR)%.o, $(SRC_FILES))
 
 CC = gcc
-CC_COMP_FLAGS = -Wall -Werror -Wextra -I$(INC_DIR) -g3 -Imlx
-CC_LINK_FLAGS = -L$(INC_DIR) -lmlx -L/usr/lib -lXext -lX11 -lm -lz
+CC_COMP_FLAGS = -Wall -Werror -Wextra -I$(INC_DIR) -Imlx -g3 -Imlx
+CC_LINK_FLAGS = -Lmlx -lmlx -L/usr/lib -lXext -lX11 -lm -lz
 NAME = cub3d
 
 all: $(NAME)
--- src/main.c
+++ src/main.c
@@ -6,42 +6,22 @@
 /*   By: samoore <samoore@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 12:18:01 by samoore           #+#    #+#             */
-/*   Updated: 2025/05/08 13:09:14 by samoore          ###   ########.fr       */
+/*   Updated: 2025/05/08 13:50:19 by samoore          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
-#include <math.h>
-#include <stdlib.h>
+#include "main.h"
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#define TEX_WIDTH 64
-#define TEX_HEIGHT 64
+int	main(void)
+{
+	t_game	game;
 
-typedef struct {
-    void *mlx;
-    void *win;
-    void *img;
-    char *img_data;
-    int bpp;
-    int size_line;
-    int endian;
-    // Add other necessary fields like player position, direction, etc.
-} t_game;
-
-int main() {
-    t_game game;
-
-    game.mlx = mlx_init();
-    game.win = mlx_new_window(game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d");
-    game.img = mlx_new_image(game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
-    game.img_data = mlx_get_data_addr(game.img, &game.bpp, &game.size_line, &game.endian);
-
-    // Load textures, initialize player position and direction, etc.
-
-    // Set up event hooks and the main loop
-    mlx_loop(game.mlx);
-
-    return 0;
+	game.mlx = mlx_init();
+	game.win = mlx_new_window(game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d");
+	game.img = mlx_new_image(game.mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
+	game.img_data = mlx_get_data_addr(
+			game.img, &game.bpp, &game.size_line, &game.endian
+			);
+	mlx_loop(game.mlx);
+	return (0);
 }
